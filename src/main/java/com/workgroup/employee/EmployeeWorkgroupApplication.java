5d455c9bb025390af10707b8879b52166c2b92c6
nikit <nikit.orekhoff@yandex.ru>
2024-05-02 09:34:03 +0300

    ДЗ 2.5

---
+++ src/main/java/com/workgroup/employee/EmployeeWorkgroupApplication.java
@@ -0,0 +1,13 @@
+package com.workgroup.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWorkgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWorkgroupApplication.class, args);
+	}
+
+}
